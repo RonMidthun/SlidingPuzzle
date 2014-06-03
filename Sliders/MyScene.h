25da4b666ba923a9d53e6c88ea77da04805ff7e7
RonMidthun <rmidthun@gmail.com>
2014-06-02 21:48:07 -0700

    Initial Commit

---
+++ Sliders/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Sliders
+//
+
+//  Copyright (c) 2014 Ronnie Midthun. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Sliders/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sliders
+//
+//  Created by Ronnie Midthun on 6/2/14.
+//  Copyright (c) 2014 Ronnie Midthun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
